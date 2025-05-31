220f9d36907c418cd721099260edc07b97952e10
rasedprogrammer <rasedprogrammer@gmail.com>
2025-05-31 22:43:45 +0600

    [DONE] navbar and category nav

--- script/index.js
+++ script/index.js
@@ -1,12 +1,20 @@
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
-    .then((data) => displayCategorise(data.categories))
+    .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error));
 };
 
-const displayCategorise = (data) => {
-  console.log(data);
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+  categories.forEach((item) => {
+    // Create Button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+    // add Button to Container
+    categoryContainer.append(button);
+  });
 };
 
 loadCategories();
